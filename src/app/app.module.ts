e95f95e3004941d8f89835442f141c2895a458a2
SinanFischer <sinanfischer@gmail.com>
2023-02-24 09:08:23 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MeComponent } from './me/me.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MeComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
